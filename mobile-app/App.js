f1c897dd35aac01033b9dd98ee07b56ba5ea852e
Wathsika <182477712+Wathsika@users.noreply.github.com>
2026-09-04 15:24:54 +0530

    feat(SPT-005): wrap App with ThemeProvider and enable automatic UI style

--- mobile-app/App.js
+++ mobile-app/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigator from './src/navigation/AppNavigator';
+import { ThemeProvider } from './src/theme/ThemeContext';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider>
       <StatusBar style="light" />
       <AppNavigator />
-    </>
+    </ThemeProvider>
   );
 }
